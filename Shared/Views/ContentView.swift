922e4762d2ac40351e25b213eb2f824d339b6eab
Vlad Kyrylenko <sorvigolovaqwerty@gmail.com>
2021-10-05 23:51:02 +0300

    KyeboardTypeChanged
    Dismiss keyboard doesn't work properly

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -22,44 +22,54 @@ struct ContentView: View {
     var textForButton = "Convert"
     
     var body: some View {
-        
         VStack{
             HStack {
                 TextFieldForNumericConversion(text: $inputNumericSystem, topLabel: textForInputNmericSystem, placeholderText: textForInputNmericSystem)
-                 // castom view
+                    .keyboardType(.decimalPad)
+                // castom view
                 TextFieldForNumericConversion(text: $inputNumber, topLabel: textForInputNmber, placeholderText: textForInputNmber)
             }.padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
             
             HStack {
                 TextFieldForNumericConversion(text: $outputNumericSystem, topLabel: textForOutputNumber, placeholderText: textForOutputNumber)
+                    .keyboardType(.decimalPad)
                 TextFieldForNumericConversion(text: $outputNumber, topLabel: textForOutputNumber, placeholderText: textForOutputNumber)
             }
             
             
             
-            HStack {
-                Button(textForButton, action: {
-                    do {
-                        outputNumber = try ConvertationHelper.numericConvertation(iNum: inputNumber,
-                                                                                  iNS: inputNumericSystem,
-                                                                                  oNS: outputNumericSystem)
-                    } catch {
-                        message = error.localizedDescription
-                        showingAlert = true
-                    }
-                }).alert(isPresented: $showingAlert, content: {
-                    Alert(
-                        title: Text("Error"),
-                        message: Text(message)
-                    )
-                })
-            }
+            Button(textForButton, action: {
+                do {
+                    outputNumber = try ConvertationHelper.numericConvertation(iNum: inputNumber,
+                        iNS: inputNumericSystem,
+                        oNS: outputNumericSystem)
+                } catch {
+                    message = error.localizedDescription
+                    showingAlert = true
+                }
+            }).alert(isPresented: $showingAlert, content: {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(message)
+                )
+            })
+            
             .padding(.top)
             
             Spacer()
+        }.onTapGesture {
+            
+            self.hideKeyboard()
         }
     }
 }
+extension View {
+    func hideKeyboard() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
